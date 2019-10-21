8622da3c58ea4924c5e0f9a3d1912b8ef5b0e0bc
Tanner Braun <tsbraun1891@gmail.com>
2019-10-21 17:55:27 -0400

    backend for first demo is finished

--- backend/api/routes/yelpAPI.js
+++ backend/api/routes/yelpAPI.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+var request = require('request');
 
-/*router.get('/', (req, res, next) =>{
-    res.status(200).json({
+// Import dotenv package which allows us to interact with .env
+const dotenv = require('dotenv');
+dotenv.config();
 
+router.get('/:location', (req, res, next) => {
+    var options = {
+        method: 'GET',
+        url: 'https://api.yelp.com/v3/businesses/search',
+        qs: { location: req.params.location },
+        headers:
+        {
+            Host: 'api.yelp.com',
+            'Postman-Token': 'c290d52d-8c00-434f-bda8-7a6c6659d46b,187ba103-eef1-4aa1-a5c7-0a95cddcddbf',
+            'Cache-Control': 'no-cache',
+            Authorization: 'Bearer 6AQnIyMC6jUeNkE4eGvqW7BSOpdLrEvp4bIgmmsN_3Vgc8tABwNQ9QHmsMavZ5Z9ZWzgoLrx30tCEXxZyLFxtbUZlBmSFrYWxhOZspIXByZJoCC-geQi1fkAXCCuXXYx'
+        },
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        // sends back the json file returned from yelp's API
+        res.status(200).json(body.businesses[0]);
+        console.log(body);
     });
-});*/
+});
 
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,18 @@
 const express = require('express');
 const app = express();
 
+// Import dotenv package which allows us to interact with .env
+const dotenv = require('dotenv');
+dotenv.config();
+
 // Just a testing port
-const port = 3000;
+const port = process.env.PORT;
 
 // Start listening on assigned port
 app.listen(port, () => {
     console.log('now listening on http://localhost:' + port)
 });
 
-const locationRoutes = require('./api/routes/products');
+const yelpAPIRoute = require('./api/routes/yelpAPI');
 
-app.use('/locations', locationRoutes);
+app.use('/yelp', yelpAPIRoute);
