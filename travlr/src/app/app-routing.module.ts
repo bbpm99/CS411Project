ecec2d6f73a8d906be18f7c26e199ee32c961cf5
Caroline Ferris <carolineferris@dhcp-wifi-8021x-168-122-218-39.bu.edu>
2019-12-08 21:11:47 -0500

    New Look and Functional Sign-In

--- travlr/src/app/app-routing.module.ts
+++ travlr/src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlaceComponent } from './place/place.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SavedComponent } from './saved/saved.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'new-itinerary',
+    component: PlaceComponent
+  },
+  {
+    path: 'signIn',
+    component: SignInComponent
+  },
+  {
+    path: 'saved-itineraries',
+    component: SavedComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- travlr/src/app/app.component.ts
+++ travlr/src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <mat-toolbar color="primary">
+    <mat-toolbar-row>
+      <h1>travlr</h1>
+      <span class="menu-spacer"></span>
+      <div>
+        <a mat-button color="accent" routerLink=""> Home </a>
+        <a mat-button color="accent" routerLink="new-itinerary"> Create a New Itinerary </a>
+        <a mat-button color="accent" routerLink="saved-itineraries"> View Saved Itineraries </a>
+      </div>
+    </mat-toolbar-row>
+    </mat-toolbar>
+    <router-outlet></router-outlet>
+    `,
+    styles: [
+      "node_modules/bootstrap/dist/css/bootstrap.css",
+      "src/styles.css"
+    ]
 })
-export class AppComponent {
-  title = 'travlr';
-}
+export class AppComponent {}
